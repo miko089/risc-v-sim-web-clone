3d3a82610e6f469a70e10885b49e30ad568b0e7f
Mikhail Maliugin <ghastmisha089@gmail.com>
2026-02-04 18:53:30 +0200

    doc comments returned

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -31,6 +31,11 @@ pub fn init_test() {
         .try_init();
 }
 
+/// Spawns a risc-v-sim-web instance, listening on the specified port.
+/// Make sure to .await the result of this function as soon as possible to
+/// avoid any weird bugs.
+/// The function returns a JoinHandle. For quick and clean test termination,
+/// make sure to [`JoinHandle::abort()`] the returned future.
 pub async fn spawn_server(span: &Span, cfg: risc_v_sim_web::Config) -> (u16, JoinHandle<()>) {
     let (port, listener) = make_listener().instrument(span.clone()).await;
     let task = tokio::spawn(risc_v_sim_web::run(span.clone(), listener, cfg));
@@ -38,6 +43,10 @@ pub async fn spawn_server(span: &Span, cfg: risc_v_sim_web::Config) -> (u16, Joi
 }
 
 async fn make_listener() -> (u16, TcpListener) {
+    // NOTE: we specifically create a listener on the same thread and make the
+    //       caller wait. This is because we want to make sure the server properly
+    //       reserves the port. Otherwise the caller's HTTP requests will race
+    //       and get a "connection refused" response.
     let address = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0);
     let listener = TcpListener::bind(address).await.unwrap();
     let port = listener.local_addr().unwrap().port();
